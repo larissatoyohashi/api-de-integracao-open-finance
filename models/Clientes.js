b33fccf361d3d7844567e460d11d2de0c991ed13
larissatoyohashi <ltoyohashi@gmail.com>
2025-09-09 13:30:50 -0300

    @larissatoyohashi

---
+++ models/Clientes.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+
+const clienteSchema = new mongoose.Schema({
+    
+
+    
+})
+
+const Cliente = mongoose.model('Cliente', clienteSchema);
+
+export default Cliente;
---
+++ models/Contas.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+
+const contaSchema = new mongoose.Schema({
+   
+    
+})
+
+const Conta = mongoose.model('Conta', contaSchema);
+
+export default Conta;
